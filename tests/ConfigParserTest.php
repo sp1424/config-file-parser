94ed4ad285df062356bff82d6bdf80e3a9670150
sp1424 <sebatianpira1999@outlook.com>
2022-04-21 22:31:23 +0100

    json and yaml tests

--- tests/ConfigParserTest.php
+++ tests/ConfigParserTest.php
@@ -73,4 +73,23 @@ class ConfigParserTest extends TestCase
 
         $this->assertTrue(json_encode($expectedOutput) === json_encode($configParser->getMergedContent()));
     }
+
+    public function testYamlAndJson()
+    {
+        $expectedOutput = [
+            'environment' => 'prod',
+            'database' => [
+                'host' => 'mysql'
+            ]
+        ];
+
+        $configParser = new ConfigParser();
+        $configParser->loadFiles(
+            __DIR__.'/testFixtures/testFile2.config.yaml',
+            __DIR__.'/testFixtures/testFile1.config.json'
+        );
+        $configParser->mergeData();
+
+        $this->assertTrue(json_encode($expectedOutput) === json_encode($configParser->getMergedContent()));
+    }
 }
